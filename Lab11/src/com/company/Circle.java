39df04cbe1a1967803ebe471313bca0671c09b12
teresabarker <48558910+teresabarker@users.noreply.github.com>
2019-03-31 12:34:04 -0400

    Update Circle.java

--- Lab11/src/com/company/Circle.java
+++ Lab11/src/com/company/Circle.java
@@ -5,15 +5,20 @@ public class Circle extends Shape {
 
     // Constructors
     public Circle() {
+        System.out.println("Circle");
         Point p1 = new Point(2,3);
         radius = 5;
-        System.out.println("Circle Point X = " + p1.x + " Y = " + p1.y);
-        System.out.println("Circle Radius R = " + radius);
+        System.out.println("Circle Points:");
+        System.out.println("   X = " + p1.x + ", Y = " + p1.y);
+        System.out.println("Circle Radius:");
+        System.out.println("   R = " + radius + "\n");
     }
 
     public Circle(Point p1) {
         radius = 5;
-        System.out.println("Circle Point X = " + p1.x + " Y = " + p1.y);
-        System.out.println("Circle Radius R = " + radius);
+        System.out.println("Circle Points:");
+        System.out.println("   X = " + p1.x + ", Y = " + p1.y);
+        System.out.println("Circle Radius:");
+        System.out.println("   R = " + radius + "\n");
     }
 }
