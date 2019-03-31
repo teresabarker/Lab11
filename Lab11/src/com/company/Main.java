b90311491da6fda58605bedf914f5c978b3c99b2
teresabarker <48558910+teresabarker@users.noreply.github.com>
2019-03-31 12:34:32 -0400

    Update Main.java

--- Lab11/src/com/company/Main.java
+++ Lab11/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Call 1. no input argument
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        Square square = new Square();
+        System.out.println(square);
+
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+
+        // Call 2. containing point values
     }
 }
