98bd868671ade22f76350f9e7d4fd9a4f4c31d09
teresabarker <48558910+teresabarker@users.noreply.github.com>
2019-03-31 12:35:55 -0400

    Update Triangle.java

--- Lab11/src/com/company/Triangle.java
+++ Lab11/src/com/company/Triangle.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Triangle extends Shape {
+
+    // Constructors
+    public Triangle() {
+        Point p1 = new Point(2,3);
+        Point p2 = new Point(4, 5);
+        Point p3 = new Point(6, 3);
+        System.out.println("Triangle");
+        System.out.println("Triangle Points:");
+        System.out.println("   X1 = " + p1.x + ", Y1 = " + p1.y);
+        System.out.println("   X2 = " + p2.x + ", Y2 = " + p2.y);
+        System.out.println("   X3 = " + p3.x + ", Y3 = " + p3.y + "\n");
+    }
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        System.out.println("Triangle");
+        System.out.println("Triangle Points:");
+        System.out.println("   X1 = " + p1.x + ", Y1 = " + p1.y);
+        System.out.println("   X2 = " + p2.x + ", Y2 = " + p2.y);
+        System.out.println("   X3 = " + p3.x + ", Y3 = " + p3.y + "\n");
+    }
 }
